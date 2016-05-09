9919c7823e0b4604b187a626e8f7572a508b40ff
nkadze <nkadze@gmail.com>
2016-05-09 22:51:14 +0400

    Add Mock test

---
+++ src/main/java/ToKillA.java
@@ -0,0 +1,9 @@
+/**
+ * Created by babdus on 5/9/16.
+ */
+public class ToKillA {
+    public String bird(int n) {
+        if(n < 0) return "ing";
+        else return "jay";
+    }
+}
--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -4,10 +4,19 @@
 
 import org.junit.Test;
 
+import static org.mockito.Mockito.*;
+
 public class UnitTest {
 
     @Test
     public void is_two_plus_two_four() {
         assert (2 + 2 == 4);
+    }
+
+    @Test
+    public void to_kill_a_mockingbird() {
+        ToKillA mocking = mock(ToKillA.class);
+        when(mocking.bird(0)).thenReturn("Harper Lee");
+        assert(mocking.bird(0) == "Harper Lee");
     }
 }

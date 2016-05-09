d7b63437afefb91e5391f3efb9a605d923591b93
nkadze <nkadze@gmail.com>
2016-05-09 22:36:28 +0400

    Add test class

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by babdus on 5/9/16.
+ */
+
+import org.junit.Test;
+
+public class UnitTest {
+
+    @Test
+    public void is_two_plus_two_four() {
+        assert (2 + 2 == 4);
+    }
+}
